fab1877cb98d2f1fe137e212c175206c1526cc9d
bijay_ps <bijay.prakash7@yahoo.in>
2023-09-21 08:28:40 +0530

    Connected node js with MySQL

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 const cors = require("cors");
+
+dotenv.config();
 
 app.use(
   cors({
@@ -8,15 +13,41 @@ app.use(
   })
 );
 
+const jsonParser = bodyParser.json();
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 const PORT = 8000;
 
-app.post("/addCity", (req, res) => {
-  console.log(req);
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+app.post("/addCity", jsonParser, async (req, res) => {
+  await insertData(req.body.city);
   res.json({
     msg: "City Added",
     status: 200,
+    city: req.body.city,
   });
 });
+
+async function insertData(cityName) {
+  return await pool.query(
+    `INSERT INTO cities (city_name) VALUES ('${cityName}');`
+  );
+}
+
+async function testDB() {
+  const [rows] = await pool.query(`select * from cities`);
+  console.log(rows);
+}
+
+// testDB();
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
